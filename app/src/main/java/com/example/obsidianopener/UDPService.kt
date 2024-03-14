980d07c410a8a655250e9069efff7465b442870b
Sorokin Oleg <sorokin.oleg.edu@gmail.com>
2024-03-14 19:35:50 +0300

    Keep service alive with notification

--- app/src/main/java/com/example/obsidianopener/UDPService.kt
+++ app/src/main/java/com/example/obsidianopener/UDPService.kt
@@ -1,11 +1,16 @@
 package com.example.obsidianopener
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import androidx.core.app.NotificationCompat
 import org.json.JSONObject
 
 class UDPService : Service() {
@@ -15,36 +20,62 @@ class UDPService : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startNotification()
         startUDP()
         return START_STICKY
     }
 
+    private fun startNotification() {
+        val channelID = "Obsidian Opener Channel"
+        val channelName = "Obsidian Opener Channel"
+        val notificationID = 6060
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelID,
+                channelName,
+                NotificationManager.IMPORTANCE_MIN,
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .build()
+
+        startForeground(notificationID, notification)
+    }
+
     private fun startUDP() {
         Thread {
-            try {
-                val bufferSize = 1024 * 5
-                val buffer = ByteArray(bufferSize)
-                val datagramSocket = java.net.DatagramSocket(6060)
-                datagramSocket.broadcast = true
-                while (running) {
-                    val packet = java.net.DatagramPacket(buffer, buffer.size)
-                    datagramSocket.receive(packet)
-                    val message = String(packet.data, 0, packet.length)
-                    val json = JSONObject(message)
-                    json.getString("filePath").let {
-                        val openMode = json.optString("openMode", "true")
-                        val line = json.optInt("line", 1)
-                        val viewMode = json.optString("viewMode", "live")
-                        val encoded: String = Uri.encode(it)
-                        Handler(Looper.getMainLooper()).post {
-                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("obsidian://advanced-uri?filepath=${encoded}&openmode=${openMode}&viewmode=${viewMode}&line=${line}"))
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            startActivity(intent)
+            while (running) {
+                try {
+                    val bufferSize = 1024 * 5
+                    val buffer = ByteArray(bufferSize)
+                    val datagramSocket = java.net.DatagramSocket(6060)
+                    datagramSocket.broadcast = true
+                    while (running) {
+                        val packet = java.net.DatagramPacket(buffer, buffer.size)
+                        datagramSocket.receive(packet)
+                        val message = String(packet.data, 0, packet.length)
+                        val json = JSONObject(message)
+                        json.getString("filePath").let {
+                            val openMode = json.optString("openMode", "true")
+                            val line = json.optInt("line", 1)
+                            val viewMode = json.optString("viewMode", "live")
+                            val encoded: String = Uri.encode(it)
+                            Handler(Looper.getMainLooper()).post {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("obsidian://advanced-uri?filepath=${encoded}&openmode=${openMode}&viewmode=${viewMode}&line=${line}"))
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                                startActivity(intent)
+                            }
                         }
                     }
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-            } catch (e: Exception) {
-                e.printStackTrace()
             }
         }.start()
     }
